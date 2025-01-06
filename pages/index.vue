3805e5609b74200a82f1e0675101608ebc41a5e6
Shafa Islam <shafaislam050@gmail.com>
2025-01-06 17:41:04 +0600

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+     
+      <header class="my-8 ">
+        <div class="header">
+            
+                <img class="mx-auto h-10 mb-2  " src="/assets/Images/Group 427322488.png" alt="" srcset="">
+            
+                
+            <div class="text-center text-[#1B4B36] font-bold text-[15px] m-5">
+                <h2 >স্বাস্থ্য, ফিটনেস এবং প্রযুক্তি সবকিছু একসাথে!</h2>
+                <h2 >"আপনার স্টাইল, আপনার স্মার্টওয়াচ।"</h2>
+            </div>
+            
+                   
+                    <p class="text-center"><span class="bg-[#EBFF3D] p-2 w-15 rounded-sm">আপনার জীবনকে আরও স্মার্ট করুন।</span></p>
+                   
+            
+                <div >
+                    <p>আপনার জীবনকে আরও স্মার্ট করুন।</p>
+                </div>
+                <!-- <img class="header-img" src="/Images/Group 427322487.png" alt="" srcset=""> -->
+                <div >
+                    <p>অর্ডার করতে ক্লিক করুন</p>
+                    <!-- <img src="/Images/Vector.png" alt="" srcset=""> -->
+                 </div>
+        </div>
+    </header>
+
+    </div>
+  </template>
+  
+  <script setup>
+  
+  </script>
+  
+  <style scoped>
+  
+  </style>
